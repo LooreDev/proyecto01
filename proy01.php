bb3b2d3c58ef45cf3ee460ab2d2f6d632f51a745
Diego <diegolorenzo.m02@gmail.com>
2023-05-29 20:09:09 -0600

    Se modifico todo el codigo

--- proy01.php
+++ proy01.php
@@ -1,18 +1,69 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $num1 = $_POST["num1"];
-    $num2 = $_POST["num2"];
-    $suma = $num1 + $num2;
-    echo "La suma de los dos números es: " . $suma;
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Suma de 2 Números</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+            padding: 20px;
+        }
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <label for="num1">Introduce el Número 1:</label>
-    <input type="number" name="num1" required><br><br>
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
 
-    <label for="num2">Introduced el Número 2:</label>
-    <input type="number" name="num2" required><br><br>
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
 
-    <input type="submit" value="Sumar">
-</form>
+        .btn-primary:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
+
+        .result {
+            margin-top: 10px;
+            padding: 10px;
+            background-color: #f0f0f0;
+            border-radius: 5px;
+        }
+
+        .div {
+            padding: 10px; 
+        }
+    </style>
+</head>
+<body> 
+    <div class="container">
+        <h1>Suma de dos números</h1>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <div class="form-group">
+                <label for="num1">Número 1:</label>
+                <input type="number" class="form-control" name="num1" id="num1" required>
+            </div>
+            <div class="div">  </div>
+            <div class="form-group">
+                <label for="num2">Número 2:</label>
+                <input type="number" class="form-control" name="num2" id="num2" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Sumar</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            $suma = $num1 + $num2;
+            echo '<div class="result">La suma de los dos números es: ' . $suma . '</div>';
+        }
+        ?>
+    </div>
+</body>
+</html>
